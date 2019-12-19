355f782e057759058002791975471c52d689fa29
chromicle <ajayprabhakar369@gmail.com>
2019-12-19 19:13:50 +0530

    FG: Added buttons and text

--- flutter_guide/lib/main.dart
+++ flutter_guide/lib/main.dart
@@ -8,6 +8,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home: Text('hello world'));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("this is the quetion"),
+            RaisedButton(child: Text("Answer 1"), onPressed: null),
+            RaisedButton(child: Text("Answer 2"), onPressed: null),
+            RaisedButton(child: Text("Answer 3"), onPressed: null),
+          ],
+        ),
+      ),
+    );
   }
 }
